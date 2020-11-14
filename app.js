bc654898428930d0118c9ef29d900b674f04ba03
Glitch (hello-sqlite) <none>
2020-11-14 00:10:47 +0000

    📙🎒 Checkpoint ./app.js:21406927/2737

--- app.js
+++ app.js
@@ -28,14 +28,111 @@ mongoose
 
 
 
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const flash = require('connect-flash');
+const session = require('express-session');
+
+const app = express();
+
+// Passport Config
+require('./config/passport')(passport);
+//require('./config/passport2')(passport);
+
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+
+
+// Connect to MongoDB
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+
+
+
+
 
 // Get the username, password, host, and databse from the .env file
-const mongoDB = ("mongodb://YI:123@cluster0-shard-00-00.5wtlj.mongodb.net:27017,cluster0-shard-00-01.5wtlj.mongodb.net:27017,cluster0-shard-00-02.5wtlj.mongodb.net:27017/comments?ssl=true&replicaSet=atlas-128v3r-shard-0&authSource=admin&retryWrites=true&w=majority");
+const mongoDB = ("mongodb+srv://"+
+                 process.env.USERNAME+
+                 ":"
+                 +process.env.PASSWORD+
+                 "@"
+                 +process.env.HOST+
+                 "/"
+                 +process.env.DATABASE);
 mongoose.connect(mongoDB,{useNewUrlParser: true, retryWrites: true});
 
 //debugging 
 mongoose.connection.on('connected', function (){
   console.log('Mongoose connected to '+process.env.DATABASE);
+});
+
+mongoose.connection.on('error', function (err){
+  console.log('Mongoose connection error: '+err);
+});
+
+mongoose.connection.on('disconnected', function (){
+  console.log('Mongoose disconnected.');
+});
+
+
+
+// EJS
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+
+// Express body parser
+app.use(express.urlencoded({ extended: true }));
+
+// Express session
+app.use(
+  session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+  })
+);
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Connect flash
+
+app.use(flash());
+
+// Global variables
+app.use(function(req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  next();
+});
+
+// Routes
+app.use('/', require('./routes/index.js'));
+app.use('/users', require('./routes/users.js'));
+app.use('/admin', require('./routes/admin.js'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
+
+// Get the username, password, host, and databse from the .env file
+const mongoDB = ("mongodb://YI:123@cluster0-shard-00-00.5wtlj.mongodb.net:27017,cluster0-shard-00-01.5wtlj.mongodb.net:27017,cluster0-shard-00-02.5wtlj.mongodb.net:27017/restaurant?ssl=true&replicaSet=atlas-128v3r-shard-0&authSource=admin&retryWrites=true&w=majority");
+mongoose.connect(mongoDB,{useNewUrlParser: true, retryWrites: true});
+
+//debugging 
+mongoose.connection.on('connected', function (){
+  console.log('Mongoose connected to ');
 });
 
 mongoose.connection.on('error', function (err){
